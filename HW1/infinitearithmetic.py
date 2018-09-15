4de1f2983510ea89666420c56708e4fc543d4cb9
Justin <jbtcao@gmail.com>
2018-09-15 15:59:21 -0500

    Corrected spacing of infinitearithmetic

--- HW1/infinitearithmetic.py
+++ HW1/infinitearithmetic.py
@@ -7,8 +7,8 @@ from BigInt import BigInt
 
 
 def infinitearithmetic():
-    path = os.path.join(os.path.dirname(__file__),'input.txt')
-    with open(path)as f:
+    path = os.path.join(os.path.dirname(__file__), 'input.txt')
+    with open(path) as f:
         lines = f.readlines()
 
     for line in lines:
@@ -18,14 +18,14 @@ def infinitearithmetic():
             x = BigInt(values[0])
             y = BigInt(values[1])
             x.multiply(y)
-            print(line.rstrip('\n')+ '='+ str(x))
+            print(line.rstrip('\n') + '=' + str(x))
 
         elif '+' in line:
             values = line.split('+')
             x = BigInt(values[0])
             y = BigInt(values[1])
             x.add(y)
-            print(line.rstrip('\n') + '='+ str(x))
+            print(line.rstrip('\n') + '=' + str(x))
 
 
 infinitearithmetic()
