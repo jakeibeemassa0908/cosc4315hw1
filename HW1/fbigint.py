4f7794c8420e53aebf23ca9fb3a79d6579e77cbb
Justin <jbtcao@gmail.com>
2018-09-18 00:43:19 -0500

    Added multiply to fbigint

--- HW1/fbigint.py
+++ HW1/fbigint.py
@@ -1,6 +1,8 @@
 """A set of functions for working with bigints.
 """
 
+from functools import reduce
+
 
 def add(bigint1, bigint2):
     nodes1 = bigint1[2]
@@ -36,6 +38,23 @@ def fromint(integer, nodesize=1):
 
 def isbigint(value):
     return isinstance(value, tuple) and len(value) == 3 and value[0] == 'bigint'
+
+
+def multiply(bigint1, bigint2):
+    nodes2 = bigint2[2]
+    new_nodesize = bigint1[1]
+
+    # For each node n, add bigint1 to itself (n - 1) times
+    multiplied = [reduce(lambda acc, _: add(acc, bigint1), range(
+        n), fromint(0, new_nodesize)) for n in nodes2]
+    # Map from bigints to nodes
+    new_nodes = [b[2] for b in multiplied]
+    # Pad extra 0s based on node offset i
+    padded = [([0] * i) + n for i, n in enumerate(new_nodes, 0)]
+    # Map from nodes to bigint
+    biginted = [new(new_nodesize, n) for n in padded]
+
+    return reduce(add, biginted)
 
 
 def new(nodesize, nodes):
